8349b023c763c1b847bfdd5fe875e7c91b92f349
Pallavi Kumari <paalavikr028@gmail.com>
2025-04-07 14:31:05 +0530

    new code

---
+++ pattern/Pattern1.java
@@ -0,0 +1,15 @@
+
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        pattern1(4);
+    }
+    static void pattern1(int n){
+        for(int r=1;r<=n;r++){
+            for(int c=1;c<=n;c++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
