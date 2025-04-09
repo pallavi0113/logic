422b6080f399c02b33c42354805b43cca37c0908
Pallavi Kumari <paalavikr028@gmail.com>
2025-04-10 00:48:34 +0530

    new code

---
+++ Student1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Student1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int totalMarks = 0;
+        int numberOfSubjects = 5;
+
+        // Input marks for 5 subjects
+        for (int i = 1; i <= numberOfSubjects; i++) {
+            System.out.print("Enter marks for subject " + i + ": ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+
+        // Calculate percentage
+        double percentage = (totalMarks / (double)(numberOfSubjects * 100)) * 100;
+        double CGPA=percentage/9.5;
+
+        // Display result
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Percentage: " + percentage + "%");
+        System.out.println("CGPA: " +CGPA);
+
+        scanner.close();
+    }
+}
+
