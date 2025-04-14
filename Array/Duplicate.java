d79e20bd6870baf2f69bee0bc4f722edc080f73e
Pallavi Kumari <paalavikr028@gmail.com>
2025-04-14 14:59:12 +0530

    new code

---
+++ Array/Duplicate.java
@@ -0,0 +1,22 @@
+//?Floyd's Tortoise and Hare (cycle Detection)
+
+
+public class Duplicate {
+    public static void main(String[] args) {
+        int [] arr={1,3,4,2,2};
+        int slow=arr[0];
+        int fast=arr[0];
+        while(true){
+            slow=arr[slow];
+            fast=arr[arr[fast]];
+            if(slow==fast){
+                break;
+            }
+        }
+        slow=arr[0];
+        while(slow!=fast){
+            slow=arr[slow];
+            fast=arr[fast];
+        }
+        System.out.println("The duplicate : " + slow);
+    }}
--- Array/Missing.java
+++ Array/Missing.java
@@ -1,12 +1,14 @@
 public class Missing {
     public static void main(String[] args) {
-        int arr[]=[1,2,4,6,3,7,8];
-        int n=arr.length;
-        int extra =n(n+1)/2;
+        int arr[]={1,2,4,6,3,7,8};
+        int n1=arr.length;
+        int n=8;
+        int extra =n*(n+1)/2;
         int sum=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n1;i++){
             sum+=arr[i];
         }
-        System.out.println("Missing number : " + return(extra-sum));
+        int missing=extra-sum;
+        System.out.println("Missing: " + missing);
     }
 }
