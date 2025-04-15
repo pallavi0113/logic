b9ae4e8236d10bd8af75f7992b7c8e8a1b03efb7
Pallavi Kumari <paalavikr028@gmail.com>
2025-04-15 12:53:29 +0530

    new code

---
+++ Array/Second.java
@@ -0,0 +1,24 @@
+public class Second {
+    public static void main(String[] args) {
+        int arr[]={12,35,1,10,34,1};
+        int first=Integer.MIN_VALUE;
+        int second=Integer.MIN_VALUE;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>first){
+                second=first;
+                first=arr[i];
+            }
+            else if(arr[i]>second && arr[i]!=first){
+                second=arr[i];
+            }
+        }
+        if(second==Integer.MIN_VALUE){
+            System.out.println("-1");
+        }
+        else{
+            System.out.println("Second: " + second);
+        }
+
+    }
+}
