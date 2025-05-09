7f28cfe19e047c0526840518c299e063df19e814
Pallavi Kumari <paalavikr028@gmail.com>
2025-05-09 17:02:15 +0530

    new code

---
+++ DoublyLinkedlist/doublyLL.java
@@ -0,0 +1,63 @@
+
+
+public class doublyLL {
+    public static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public static void display(Node head){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+    public static void displayrev(Node tail){
+        Node temp=tail;
+        while(temp !=null){
+            System.out.print(temp.data+" ");
+            temp=temp.prev;
+        }
+        System.out.println();
+    }
+    public static void display2(Node random){
+        Node temp=random;
+        while(temp.prev!=null){
+            temp=temp.prev;
+        }
+        // now temp is the head of the list
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String [] arg){
+        Node a=new Node(4);
+        Node b=new Node(10);
+        Node c=new Node(2);
+        Node d=new Node(99);
+        Node e=new Node(13);
+        a.prev=null;
+        a.next=b;
+        b.prev=a;
+        b.next=c;
+        c.prev=b;
+        c.next=d;
+        d.prev=c;
+        d.next=e;
+        e.prev=d;
+        e.next=null;
+        display(a);
+        displayrev(e);
+        display2(c);
+    }
+}
