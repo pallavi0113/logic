48c3c18519b5e28ba4feda664285f3997c119116
Pallavi Kumari <paalavikr028@gmail.com>
2025-07-09 23:49:26 +0530

    new code

---
+++ Revise/Largets.java
@@ -0,0 +1,16 @@
+
+import java.util.Arrays;
+
+class Largets{
+    public static void main(String[] args) {
+        int [] arr1={2,5,3,7,4};
+        System.out.println("Largest number : " + sort(arr1));
+        int [] arr2={8,9,5,10,3};
+        System.out.println("Largest number is:" + sort(arr2));
+
+    }
+    static int sort(int[] arr){
+            Arrays.sort(arr);
+            return arr[arr.length-1];
+        }
+}
