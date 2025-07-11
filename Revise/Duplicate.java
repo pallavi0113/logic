62676c045232a69deb0a8c9d14dbfd5ff0517a71
Pallavi Kumari <paalavikr028@gmail.com>
2025-07-12 00:02:51 +0530

    new code

---
+++ Revise/Duplicate.java
@@ -0,0 +1,20 @@
+class Duplicate{
+    public static void main(String [] arg){
+        int arr[]={1,1,2,2,2,3,3};
+        int k=remove(arr);
+        System.out.println("the array after remove: ");
+        for(int i=0;i<k;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    static int remove(int[] arr){
+        int i=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[i] != arr[j]){
+                i++;
+                arr[i]=arr[j];
+            }
+        }
+        return i+1;
+    }
+}
