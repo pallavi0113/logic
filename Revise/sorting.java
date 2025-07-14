7b07eeb5aaf36249ce309e61ecd33264f4ffa94b
Pallavi Kumari <paalavikr028@gmail.com>
2025-07-14 23:14:08 +0530

    new code

--- Revise/sorting.java
+++ Revise/sorting.java
@@ -5,9 +5,12 @@ public class sorting{
     public static void sortt(ArrayList<Integer> arr, int n){
         int cnt0=0,cnt1=0,cnt2=0;
         for(int i=0;i<n;i++){
-            if(arr.get(i) == 0) cnt0++;
-            else if(arr.get(i) ==1) cnt1++;
-            else cnt2++;
+            if(null == arr.get(i)) cnt2++;
+            else switch (arr.get(i)) {
+                case 0 -> cnt0++;
+                case 1 -> cnt1++;
+                default -> cnt2++;
+            }
         }
         for (int i=0;i<cnt0;i++) arr.set(i,0);
         for(int i=cnt0;i<cnt0+cnt1;i++) arr.set(i,1);
