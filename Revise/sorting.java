6969b201fc5df28acd356b2774173aee80995891
Pallavi Kumari <paalavikr028@gmail.com>
2025-07-15 00:36:03 +0530

    new code

--- Revise/sorting.java
+++ Revise/sorting.java
@@ -2,21 +2,51 @@
 import java.util.*;
 
 public class sorting{
-    public static void sortt(ArrayList<Integer> arr, int n){
-        int cnt0=0,cnt1=0,cnt2=0;
-        for(int i=0;i<n;i++){
-            if(null == arr.get(i)) cnt2++;
-            else switch (arr.get(i)) {
-                case 0 -> cnt0++;
-                case 1 -> cnt1++;
-                default -> cnt2++;
+    // public static void sortt(ArrayList<Integer> arr, int n){
+    //     int cnt0=0,cnt1=0,cnt2=0;
+    //     for(int i=0;i<n;i++){
+    //         if(null == arr.get(i)) cnt2++;
+    //         else switch (arr.get(i)) {
+    //             case 0 -> cnt0++;
+    //             case 1 -> cnt1++;
+    //             default -> cnt2++;
+    //         }
+    //     }
+    //     for (int i=0;i<cnt0;i++) arr.set(i,0);
+    //     for(int i=cnt0;i<cnt0+cnt1;i++) arr.set(i,1);
+    //     for(int i=cnt0+cnt1;i<n;i++) arr.set(i,2);
+    // }
+
+
+
+    public static void sortt(ArrayList<Integer> arr, int n) {
+    int low = 0, mid = 0, high = n - 1;
+    while (mid <= high) {
+        if (arr.get(mid) == null) {
+            Integer temp = arr.get(mid);
+            arr.set(mid, arr.get(high));
+            arr.set(high, temp);
+            high--;
+        } else {
+            switch (arr.get(mid)) {
+                case 0 -> {
+                    int temp = arr.get(low);
+                    arr.set(low, arr.get(mid));
+                    arr.set(mid, temp);
+                    low++;
+                    mid++;
+                }
+                case 1 -> mid++;
+                default -> {
+                    int temp = arr.get(mid);
+                    arr.set(mid, arr.get(high));
+                    arr.set(high, temp);
+                    high--;
+                }
             }
         }
-        for (int i=0;i<cnt0;i++) arr.set(i,0);
-        for(int i=cnt0;i<cnt0+cnt1;i++) arr.set(i,1);
-        for(int i=cnt0+cnt1;i<n;i++) arr.set(i,2);
     }
-
+}
     public static void main(String[] args) {
         int n=6;
         ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(new Integer[] {0,2,1,2,0,1}));
