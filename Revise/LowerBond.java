909c4d3d994e2e2ca80b5fdd62c68b58ddaa0634
Pallavi Kumari <paalavikr028@gmail.com>
2025-07-18 00:23:39 +0530

    new code

---
+++ Revise/LowerBond.java
@@ -0,0 +1,25 @@
+class LowerBond{
+    public static int search(int [] arr, int target){
+        int low=0,high=arr.length-1;
+        int ans=arr.length;
+        while(low<=high){
+        int mid=low+(high-low)/2;
+
+        if(arr[mid]>=target){
+            ans= mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+        }
+        return ans;
+        
+    }
+    public static void main(String[] args) {
+        int arr[]={3,4,6,7,8,10,11};
+        int x=2;
+        int ans=search(arr,x);
+        System.out.println("Lowerbond is :" + ans);
+    }
+}
