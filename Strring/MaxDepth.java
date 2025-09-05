1b2249d2d2a96f677e8a7800a1e9d7e2bbdcfca3
Pallavi Kumari <paalavikr028@gmail.com>
2025-09-06 00:44:46 +0530

    new code

--- Strring/MaxDepth.java
+++ Strring/MaxDepth.java
@@ -1,18 +1,31 @@
 class MaxDepth {
     public static int maxDepth(String s) {
-         int maxDepth = 0;
-    int currentDepth = 0;
+    //      int maxDepth = 0;
+    // int currentDepth = 0;
 
-    for (char c : s.toCharArray()) {
-        if (c == '(') {
-            currentDepth++;
-            maxDepth = Math.max(maxDepth, currentDepth);
-        } else if (c == ')') {
-            currentDepth--;
+    // for (char c : s.toCharArray()) {
+    //     if (c == '(') {
+    //         currentDepth++;
+    //         maxDepth = Math.max(maxDepth, currentDepth);
+    //     } else if (c == ')') {
+    //         currentDepth--;
+    //     }
+    // }
+
+    // return maxDepth;
+
+        int maxD=0;
+        int currD=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                currD++;
+                maxD=Math.max(maxD,currD);
+            }
+            else if(s.charAt(i)==')'){
+                currD--;
+            }
         }
-    }
-
-    return maxDepth;
+        return maxD;
     }
 
     public static void main(String[] args) {
